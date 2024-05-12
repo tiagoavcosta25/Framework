f2e882d18cc50413a51e1aeae2d4e43494a31d2c
tiagoavcosta25 <tiagoavcosta25@gmail.com>
2024-05-12 16:01:15 +0100

    feat: add DTO and Utility annotations

---
+++ src/main/java/org/framework/annotations/Utility.java
@@ -0,0 +1,14 @@
+package org.framework.annotations;
+
+import org.springframework.stereotype.Component;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Component
+public @interface Utility {
+    String value() default "";
+}
